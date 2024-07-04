19fafa29b30d92c071f0e06187a3e00d6d5918e0
TerryTess <terrywamboi.ke@gmail.com>
2024-07-04 16:49:23 +0300

    Top App Bar

---
+++ Components/app/src/main/java/com/example/components/TopAppBar.kt
@@ -0,0 +1,20 @@
+package com.example.components
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TopAppBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_top_app_bar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
